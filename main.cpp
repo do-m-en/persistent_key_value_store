49932aae8c62fe399ada0e340a2a5e9476d475b9
Domen Vrankar <domen.vrankar@gmail.com>
2024-02-04 16:20:32 +0100

    rewrite to use seastar http engine

--- main.cpp
+++ main.cpp
@@ -1,74 +1,135 @@
 #include <seastar/core/app-template.hh>
-#include <seastar/core/reactor.hh>
+#include <seastar/core/coroutine.hh>
+#include <seastar/core/reactor.hh> // seastar::condition_variable
+#include <seastar/http/function_handlers.hh>
+#include <seastar/http/httpd.hh>
+#include <seastar/http/routes.hh>
+
 #include <iostream>
 
-seastar::future<> handle_connection
-(
-  seastar::connected_socket& socket,
-  seastar::socket_address const& address
-)
+namespace
 {
-  const char* canned_response = "Seastar is the future!\n";
+  class stop_signal
+  {
+  public:
+    stop_signal()
+    {
+      seastar::engine().handle_signal(
+        SIGINT,
+        [this] { signaled(); });
+      seastar::engine().handle_signal(
+        SIGTERM,
+        [this] { signaled(); });
+    }
 
-  std::cout << "Accepted connection from " << address << "\n";
+    ~stop_signal()
+    {
+      seastar::engine().handle_signal(SIGINT, [] {});
+      seastar::engine().handle_signal(SIGTERM, [] {});
+    }
 
-  auto out = socket.output();
+    seastar::future<> wait() { return cv_.wait([this] { return caught_; }); }
+    bool stopping() const { return caught_; }
 
-  co_await out.write(canned_response);
-  co_await out.close();
-}
+  private:
+    void signaled()
+    {
+      if( caught_ )
+        return;
 
-seastar::future<> service_loop()
-{
-  return
-    seastar::do_with(
-      seastar::listen(
-        seastar::make_ipv4_address({8080}),
-        []
-        {
-          // support fast server restarts by allowing reuse the local address port
-          // otherwise server needs to wait for the connections that were already
-          // closed but need to waith for the TIME_WAIT duration before freeing the port
-          seastar::listen_options lo;
-          lo.reuse_address = true;
+      caught_ = true;
+      cv_.broadcast();
+    }
 
-          return lo;
-        }()),
-      []( auto& listener )
-      {
-        return
-          seastar::keep_doing(
-            [&listener]() -> seastar::future<>
-            {
-              auto res = co_await listener.accept();
-              try
-              {
-                co_await handle_connection( res.connection, res.remote_address );
-              }
-              catch( ... )
-              {
-                fmt::print(
-                  stderr,
-                  "Could not handle connection: {}\n",
-                  std::current_exception());
-              }
-            });
-      });
+    bool caught_ = false;
+    seastar::condition_variable cv_;
+  };
+
+  seastar::future<> service_loop( uint16_t port )
+  {
+    stop_signal stop_signal;
+
+    seastar::httpd::http_server_control http_server;
+
+    std::cout << "server start\n";
+    co_await http_server.start( "pkvs" );
+
+    std::cout << "setting routes\n";
+    co_await
+      http_server
+        .set_routes(
+          []( seastar::httpd::routes& r )
+          {
+            r.add(
+              seastar::httpd::operation_type::GET,
+              seastar::httpd::url("/get"),
+              new seastar::httpd::function_handler(
+                []( seastar::httpd::const_req req )
+                {
+                  std::cout << req.content << std::endl; // FIXME server-side deprecated: use content_stream instead
+
+                  return "get";
+                }));
+
+            r.add(
+              seastar::httpd::operation_type::POST,
+              seastar::httpd::url("/post"),
+              new seastar::httpd::function_handler(
+                []( seastar::httpd::const_req req )
+                {
+                  std::cout << req.content << std::endl; // FIXME server-side deprecated: use content_stream instead
+
+                  return "post";
+                }));
+
+            r.add(
+              seastar::httpd::operation_type::POST,
+              seastar::httpd::url("/delete"),
+              new seastar::httpd::function_handler(
+                []( seastar::httpd::const_req req )
+                {
+                  std::cout << req.content << std::endl; // FIXME server-side deprecated: use content_stream instead
+
+                  return "delete";
+                }));
+          });
+
+    std::cout << "listening on port " << port << '\n';
+    co_await http_server.listen(seastar::ipv4_addr("0.0.0.0", port));
+
+    co_await stop_signal.wait();
+
+    std::cout << "shutting down\n";
+    co_await http_server.stop(); // TODO RAII
+  }
 }
 
 int main( int argc, char** argv )
 {
-    seastar::app_template app;
+  seastar::app_template app;
+
+  app.add_options()(
+    "port,p",
+    boost::program_options::value<uint16_t>()->default_value( 8080 ),
+    "HTTP Server port");
+
+  try
+  {
     app.run(
       argc,
       argv,
-      [] -> seastar::future<>
+      [ &app ]
       {
-        return
-          seastar::parallel_for_each(boost::irange<unsigned>(0, seastar::smp::count),
-            [](unsigned c)
-            {
-              return seastar::smp::submit_to( c, service_loop );
-            });
+        auto&& configuration = app.configuration();
+        return service_loop( configuration["port"].as<uint16_t>() );
       });
+  }
+  catch (...)
+  {
+      std::cerr << "Failed to start: " << std::current_exception() << '\n';
+
+      return 1;
+  }
+
+  return 0;
 }
