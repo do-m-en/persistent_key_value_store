9c0c1c4b102577c6ce83cc666642aa374e93ca05
Domen Vrankar <domen.vrankar@gmail.com>
2024-02-11 00:55:19 +0100

    improved exception handling
    From some parts of the code didn't stop the server beforehand
    so you didn't get the exception output.

--- main.cpp
+++ main.cpp
@@ -75,230 +75,231 @@ namespace
     std::cout << "running on: " << seastar::smp::count << '\n';
 
     co_await store.start();
-    co_await store.invoke_on_all(
-      [ memtable_memory_footprint_eviction_threshold ]( pkvs::pkvs_shard& local_shard )
-      {
-        return local_shard.run( memtable_memory_footprint_eviction_threshold );
-      });
 
     seastar::httpd::http_server_control http_server;
 
     std::cout << "server start\n";
     co_await http_server.start( "pkvs" );
 
-    std::cout << "setting routes\n";
-    co_await
-      http_server
-        .set_routes(
-          [ &store ]( seastar::httpd::routes& r )
-          {
-            auto common_request_processing =
-              []
-              (
-                seastar::http::request& req,
-                std::unordered_set< std::string > expected_keys
-              )
-                ->
-                  std::expected
-                  <
-                    std::tuple
-                    <
-                      nlohmann::json,
-                      size_t
-                    >,
-                    std::string
-                  >
-              {
-                  // FIXME server-side deprecated: use content_stream instead
-                  // throws if data is not in utf-8 format
-                  try
-                  {
-                    nlohmann::json data = nlohmann::json::parse( req.content.c_str() );
-
-                    if( expected_keys.contains( "key" ) == false )
-                      return std::unexpected("{\"result\":\"internal server error\"}");
-
-                    for( auto& [key, val] : data.items() )
-                    {
-                      if
-                      (
-                        expected_keys.erase( key ) == 0 ||
-                        val.type() != nlohmann::json::value_t::string
-                      )
-                      {
-                        return std::unexpected("{\"result\":\"request error\"}");
-                      }
-                    }
-
-                    if( expected_keys.empty() == false )
-                      return std::unexpected("{\"result\":\"request error\"}");
-
-                    auto key = data["key"].template get<std::string_view>();
-
-                    if( key.empty() || key.size() > 256 )
-                      return std::unexpected("{\"result\":\"invalid key size\"}");
-
-                    size_t shard_no = pkvs::key_to_shard_no( key );
-
-                    return std::tuple{ std::move( data ), shard_no };
-                  }
-                  catch( ... )
-                  {
-                    return std::unexpected("{\"result\":\"request error\"}");
-                  }
-              };
-            r.add(
-              seastar::httpd::operation_type::GET,
-              seastar::httpd::url("/get"),
-              new seastar::httpd::function_handler(
-                [ &store, common_request_processing ]
-                (
-                  std::unique_ptr<seastar::http::request> req
-                ) -> seastar::future<seastar::json::json_return_type>
-                {
-                  auto processed = common_request_processing( *req, { "key" } );
-
-                  if( processed.has_value() == false )
-                    co_return processed.error();
-
-                  auto const& [ data, shard_no ] = *processed;
-
-                  auto result =
-                    co_await
-                      store.invoke_on(
-                        shard_no,
-                        [key = data["key"].template get<std::string_view>()]
-                        (
-                          pkvs::pkvs_shard& local_shard
-                        )
-                        {
-                          return local_shard.get_item( key );
-                        });
-
-                  if( result != std::nullopt )
-                    co_return "{\"value\":\"" + result.value() + "\"}";
-
-                  co_return "{\"result\":\"missing\"}";
-                }));
-
-            r.add(
-              seastar::httpd::operation_type::POST,
-              seastar::httpd::url("/post"),
-              new seastar::httpd::function_handler(
-                [ &store, common_request_processing ]
-                (
-                  std::unique_ptr<seastar::http::request> req
-                ) -> seastar::future<seastar::json::json_return_type>
-                {
-                  auto processed = common_request_processing( *req, { "key", "value" } );
-
-                  if( processed.has_value() == false )
-                    co_return processed.error();
-
-                  auto const& [ data, shard_no ] = *processed;
-
-                  co_await
-                    store.invoke_on(
-                      shard_no,
-                      [
-                        key = data["key"].template get<std::string_view>(),
-                        value = data["value"].template get<std::string_view>()
-                      ]
-                      (
-                        pkvs::pkvs_shard& local_shard
-                      )
-                      {
-                        return local_shard.insert_item( key, value );
-                      });
-
-                  co_return "{\"result\":\"ok\"}";
-                }));
-
-            r.add(
-              seastar::httpd::operation_type::POST,
-              seastar::httpd::url("/delete"),
-              new seastar::httpd::function_handler(
-                [ &store, common_request_processing ]
-                (
-                  std::unique_ptr<seastar::http::request> req
-                ) -> seastar::future<seastar::json::json_return_type>
-                {
-                  auto processed = common_request_processing( *req, { "key" } );
-
-                  if( processed.has_value() == false )
-                    co_return processed.error();
-
-                  auto const& [ data, shard_no ] = *processed;
-
-                  co_await
-                    store.invoke_on(
-                      shard_no,
-                      [key = data["key"].template get<std::string_view>()]
-                      (
-                        pkvs::pkvs_shard& local_shard
-                      )
-                      {
-                        return local_shard.delete_item( key );
-                      });
-
-                  co_return "{\"result\":\"ok\"}";
-                }));
-
-            r.add(
-              seastar::httpd::operation_type::GET,
-              seastar::httpd::url("/sorted_keys"),
-              new seastar::httpd::function_handler(
-                [ &store, common_request_processing ]
-                (
-                  std::unique_ptr<seastar::http::request> req
-                ) -> seastar::future<seastar::json::json_return_type>
-                {
-                  std::set< std::string > keys;
-
-                  try
-                  {
-                    co_await seastar::coroutine::parallel_for_each(
-                      std::views::iota( 0u, seastar::smp::count ),
-                      [ &store, &keys ]( size_t shard_no ) -> seastar::future<>
-                      {
-                        keys.merge(
-                          co_await
-                            store.invoke_on(
-                              shard_no,
-                              []( pkvs::pkvs_shard& local_shard )
-                              {
-                                return local_shard.sorted_keys();
-                              }));
-                      });
-                  }
-                  catch( ... )
-                  {
-                    std::cerr << "keys failed: " << std::current_exception() << '\n';
-
-                    co_return "{\"result\":\"internal server error\"}";
-                  }
-
-                  std::string result{ "{\"keys\":[" };
-
-                  for( auto const& key : keys )
-                    result += '"' + key + "\",";
-
-                  if( keys.empty() == false )
-                    result.pop_back();
-
-                  result += "]}";
-
-                  co_return result;
-                }));
-          });
-
-    std::cout << "try listening on port " << port << '\n';
-    co_await http_server.listen(seastar::ipv4_addr("0.0.0.0", port));
-    std::cout << "listening\n";
-
     co_await
       [&] -> seastar::future<>
       {
+        co_await store.invoke_on_all(
+          [ memtable_memory_footprint_eviction_threshold ]( pkvs::pkvs_shard& local_shard )
+          {
+            return local_shard.run( memtable_memory_footprint_eviction_threshold );
+          });
+
+        std::cout << "setting routes\n";
+        co_await
+          http_server
+            .set_routes(
+              [ &store ]( seastar::httpd::routes& r )
+              {
+                auto common_request_processing =
+                  []
+                  (
+                    seastar::http::request& req,
+                    std::unordered_set< std::string > expected_keys
+                  )
+                    ->
+                      std::expected
+                      <
+                        std::tuple
+                        <
+                          nlohmann::json,
+                          size_t
+                        >,
+                        std::string
+                      >
+                  {
+                      // FIXME server-side deprecated: use content_stream instead
+                      // throws if data is not in utf-8 format
+                      try
+                      {
+                        nlohmann::json data = nlohmann::json::parse( req.content.c_str() );
+
+                        if( expected_keys.contains( "key" ) == false )
+                          return std::unexpected("{\"result\":\"internal server error\"}");
+
+                        for( auto& [key, val] : data.items() )
+                        {
+                          if
+                          (
+                            expected_keys.erase( key ) == 0 ||
+                            val.type() != nlohmann::json::value_t::string
+                          )
+                          {
+                            return std::unexpected("{\"result\":\"request error\"}");
+                          }
+                        }
+
+                        if( expected_keys.empty() == false )
+                          return std::unexpected("{\"result\":\"request error\"}");
+
+                        auto key = data["key"].template get<std::string_view>();
+
+                        if( key.empty() || key.size() > 256 )
+                          return std::unexpected("{\"result\":\"invalid key size\"}");
+
+                        size_t shard_no = pkvs::key_to_shard_no( key );
+
+                        return std::tuple{ std::move( data ), shard_no };
+                      }
+                      catch( ... )
+                      {
+                        return std::unexpected("{\"result\":\"request error\"}");
+                      }
+                  };
+                r.add(
+                  seastar::httpd::operation_type::GET,
+                  seastar::httpd::url("/get"),
+                  new seastar::httpd::function_handler(
+                    [ &store, common_request_processing ]
+                    (
+                      std::unique_ptr<seastar::http::request> req
+                    ) -> seastar::future<seastar::json::json_return_type>
+                    {
+                      auto processed = common_request_processing( *req, { "key" } );
+
+                      if( processed.has_value() == false )
+                        co_return processed.error();
+
+                      auto const& [ data, shard_no ] = *processed;
+
+                      auto result =
+                        co_await
+                          store.invoke_on(
+                            shard_no,
+                            [key = data["key"].template get<std::string_view>()]
+                            (
+                              pkvs::pkvs_shard& local_shard
+                            )
+                            {
+                              return local_shard.get_item( key );
+                            });
+
+                      if( result != std::nullopt )
+                        co_return "{\"value\":\"" + result.value() + "\"}";
+
+                      co_return "{\"result\":\"missing\"}";
+                    }));
+
+                r.add(
+                  seastar::httpd::operation_type::POST,
+                  seastar::httpd::url("/post"),
+                  new seastar::httpd::function_handler(
+                    [ &store, common_request_processing ]
+                    (
+                      std::unique_ptr<seastar::http::request> req
+                    ) -> seastar::future<seastar::json::json_return_type>
+                    {
+                      auto processed = common_request_processing( *req, { "key", "value" } );
+
+                      if( processed.has_value() == false )
+                        co_return processed.error();
+
+                      auto const& [ data, shard_no ] = *processed;
+
+                      co_await
+                        store.invoke_on(
+                          shard_no,
+                          [
+                            key = data["key"].template get<std::string_view>(),
+                            value = data["value"].template get<std::string_view>()
+                          ]
+                          (
+                            pkvs::pkvs_shard& local_shard
+                          )
+                          {
+                            return local_shard.insert_item( key, value );
+                          });
+
+                      co_return "{\"result\":\"ok\"}";
+                    }));
+
+                r.add(
+                  seastar::httpd::operation_type::POST,
+                  seastar::httpd::url("/delete"),
+                  new seastar::httpd::function_handler(
+                    [ &store, common_request_processing ]
+                    (
+                      std::unique_ptr<seastar::http::request> req
+                    ) -> seastar::future<seastar::json::json_return_type>
+                    {
+                      auto processed = common_request_processing( *req, { "key" } );
+
+                      if( processed.has_value() == false )
+                        co_return processed.error();
+
+                      auto const& [ data, shard_no ] = *processed;
+
+                      co_await
+                        store.invoke_on(
+                          shard_no,
+                          [key = data["key"].template get<std::string_view>()]
+                          (
+                            pkvs::pkvs_shard& local_shard
+                          )
+                          {
+                            return local_shard.delete_item( key );
+                          });
+
+                      co_return "{\"result\":\"ok\"}";
+                    }));
+
+                r.add(
+                  seastar::httpd::operation_type::GET,
+                  seastar::httpd::url("/sorted_keys"),
+                  new seastar::httpd::function_handler(
+                    [ &store, common_request_processing ]
+                    (
+                      std::unique_ptr<seastar::http::request> req
+                    ) -> seastar::future<seastar::json::json_return_type>
+                    {
+                      std::set< std::string > keys;
+
+                      try
+                      {
+                        co_await seastar::coroutine::parallel_for_each(
+                          std::views::iota( 0u, seastar::smp::count ),
+                          [ &store, &keys ]( size_t shard_no ) -> seastar::future<>
+                          {
+                            keys.merge(
+                              co_await
+                                store.invoke_on(
+                                  shard_no,
+                                  []( pkvs::pkvs_shard& local_shard )
+                                  {
+                                    return local_shard.sorted_keys();
+                                  }));
+                          });
+                      }
+                      catch( ... )
+                      {
+                        std::cerr << "keys failed: " << std::current_exception() << '\n';
+
+                        co_return "{\"result\":\"internal server error\"}";
+                      }
+
+                      std::string result{ "{\"keys\":[" };
+
+                      for( auto const& key : keys )
+                        result += '"' + key + "\",";
+
+                      if( keys.empty() == false )
+                        result.pop_back();
+
+                      result += "]}";
+
+                      co_return result;
+                    }));
+              });
+
+        std::cout << "try listening on port " << port << '\n';
+        co_await http_server.listen(seastar::ipv4_addr("0.0.0.0", port));
+        std::cout << "listening\n";
+
         while( signal.stopping() == false )
         {
           co_await seastar::sleep( std::chrono::seconds( 1 ) );
